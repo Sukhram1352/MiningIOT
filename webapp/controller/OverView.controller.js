b51c688f7e90dd626abc33b76d8a82c0cf92ca17
Sukhram Bhamboo <sukhram.bhamboo@sap.com>
2019-09-06 07:30:17 +0000

    latest changes

--- webapp/controller/OverView.controller.js
+++ webapp/controller/OverView.controller.js
@@ -327,7 +327,8 @@ sap.ui.define([
                 	if(oData.Nplda && oData.Nplda.getTime() > 0) {
                 		MessageBox.show("Information", {
 							icon: MessageBox.Icon.INFORMATION,
-							title: "Maintenance order " + oData.Warpl + " for machine " + this.getMachineName(oData.Equnr) + " is scheduled on " + oData.Nplda,
+							title: "Information",
+							details: "Maintenance order " + oData.Warpl + " for machine " + this.getMachineName(oData.Equnr) + " is scheduled on " + oData.Nplda,
 							actions: [MessageBox.Action.OK]
 						});
                 	} else {
